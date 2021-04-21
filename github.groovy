eb509d6e38c4a4f0fc68c74e80ea6e603aa7a775
aravindhsz <68924518+aravindhsz@users.noreply.github.com>
2021-04-21 13:38:47 +0530

    Update github.groovy

--- github.groovy
+++ github.groovy
@@ -1,1 +1,12 @@
 
+import groovy.json.JsonSlurper
+
+def getPrBody(){
+  int exitcode = 0
+for (slave in hudson.model.Hudson.instance.slaves) {
+ if (slave.getComputer().isOffline().toString() == "true"){
+ println('Slave ' + slave.name + " is offline!"); 
+ exitcode++;
+ }
+}
+}
