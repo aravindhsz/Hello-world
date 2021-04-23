481ebad9aee96eca925b0229c7e2f188899c9420
aravindhsz <68924518+aravindhsz@users.noreply.github.com>
2021-04-23 12:39:28 +0530

    Update func.groovy

--- func.groovy
+++ func.groovy
@@ -1,13 +1,17 @@
 
 import hudson.model.*    
 import jenkins.security.*
+import jenkins.model.*
 import hudson.security.*
 
   def rest(){
     def jobNames = []
-    hudson.model.Hudson.instance.getView('VIEW_NAME').items.each() { 
+   /* hudson.model.Hudson.instance.getView('VIEW_NAME').items.each() { 
       jobNames.add(it.fullDisplayName) 
-    }
+    }*/
+    Jenkins.instance.getAllItems(AbstractItem.class).each { println(it.fullName) };
+
+
      
   }
 return this
