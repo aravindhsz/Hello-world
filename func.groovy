4911e52bf22a6d9c5d81512461e96118b4ca938f
aravindhsz <68924518+aravindhsz@users.noreply.github.com>
2021-04-23 12:24:11 +0530

    Update func.groovy

--- func.groovy
+++ func.groovy
@@ -1,1 +1,24 @@
 
+import hudson.model.*    
+import jenkins.security.*
+import hudson.security.*
+
+  def rest(){
+    def jobNames = []
+    hudson.model.Hudson.instance.getView('VIEW_NAME').items.each() { 
+      jobNames.add(it.fullDisplayName) 
+    }
+
+    def userIDs = ['user1_ID','user2_ID', 'user3_ID']
+
+    // For each project
+    for(item in Hudson.instance.items) {
+      for(jobName in jobNames){
+        if(item.name.equalsIgnoreCase(jobName))
+        {
+          println(item.name)
+
+        }
+      }
+    }
+  }
