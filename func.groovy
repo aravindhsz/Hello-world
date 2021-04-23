a9d992fe3735d62e68658d652f5fbffcc373049c
aravindhsz <68924518+aravindhsz@users.noreply.github.com>
2021-04-23 18:10:31 +0530

    Update func.groovy

--- func.groovy
+++ func.groovy
@@ -20,8 +20,8 @@ for(item in Hudson.instance.items) {
     {
         Set<String> users = new HashSet<>();
       //adding the users to give access
-        users.add('user1');
-      echo "providing access of the job:${jobName} to the user:user1"
+        users.add('user2');
+      echo "providing access of the job:${jobName} to the user:user2"
      // println(item+"to user1")
       
       
